ceef38e0f7a59e1ba55723200643c130c188541e
Marzio Costantini <costantinimarzio@gmail.com>
2024-09-16 13:42:33 +0200

    use Open Wearteh API

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -5,8 +5,6 @@ import { useLocation } from 'react-router-dom';
 import { LatLngExpression } from 'leaflet';
 import Loader from '../Loader/Loader';
 
-
-
 interface ICity {
     stadt: string[];
 }
@@ -19,54 +17,64 @@ interface Icoords {
 
 const MapWithMarkers: React.FC<ICity> = ({ stadt }) => {
     const [coordinates, setCoordinates] = useState<Icoords[]>([]);
+    const [error, setError] = useState<string | null>(null); // Fehlerzustand hinzufügen
+    const location = useLocation();
 
-
-
-
-    console.log(coordinates);
-
-
-    // holt location
-    const location = useLocation()
-
+    const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 
     // Koordinaten Funktion
     useEffect(() => {
         async function getCoordinates(cityName: string): Promise<Icoords> {
-            const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(cityName)}`);
-            const data = await response.json();
+            try {
+                const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(cityName)}&limit=1&appid=${apiKey}`);
 
-            if (data && data.length > 0) {
-                const location = data[0];
-                return { lat: parseFloat(location.lat), lng: parseFloat(location.lon), name: cityName };
-            } else {
-                throw new Error('Geocoding nicht gefunden');
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch coordinates for ${cityName}`);
+                }
+
+                const data = await response.json();
+
+                if (data && data.length > 0) {
+                    const location = data[0];
+                    return { lat: location.lat, lng: location.lon, name: cityName };
+                } else {
+                    throw new Error(`No results found for ${cityName}`);
+                }
+            } catch (error: any) {
+                console.error(`Error fetching data for ${cityName}:`, error.message);
+                setError(error.message);
+                throw error;
             }
         }
 
         const fetchAllCoordinates = async () => {
-            const allCoords = await Promise.all(stadt.map(city => getCoordinates(city)));
-            setCoordinates(allCoords);
+            try {
+                const allCoords = await Promise.all(stadt.map(city => getCoordinates(city)));
+                setCoordinates(allCoords);
+            } catch (error) {
+                console.error(error);
+            }
         };
 
-        fetchAllCoordinates().catch(error => {
-            console.error(error);
-        });
-
+        fetchAllCoordinates();
     }, [stadt]);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
     if (coordinates.length === 0) {
         return <Loader />;
     }
 
-    // Zoom Level der Karte anahand von
-    const center = location.pathname === "/my-bookings"
+    // Zoom Level der Karte anhand von Koordinaten
+    const center: LatLngExpression = location.pathname === "/my-bookings"
         ? [coordinates[0].lat, coordinates[0].lng]
-        : [51.1657, 10.4515];
+        : [51.1657, 10.4515]; // Standard für Deutschland
 
     return (
         <MapContainer
-            center={center as LatLngExpression}
+            center={center}
             zoom={6}
             style={{ width: '100%', height: '400px' }}
             className='map'
