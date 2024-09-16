8dd810031c9cea974f822300f665b62f5ce234da
Marzio Costantini <costantinimarzio@gmail.com>
2024-09-16 14:41:35 +0200

    de sha

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -9,7 +9,7 @@ import Loader from '../Loader/Loader';
 const customIcon = new Icon({
     iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
     iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    // shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
